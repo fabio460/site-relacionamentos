5746fe398351224e4976f4155bf809a450bdddce
unknown <dasilvadeoliveirafabio@gmail.com>
2022-09-30 18:20:49 -0300

    tela de login

--- src/Cadastro/Cadastro.js
+++ src/Cadastro/Cadastro.js
@@ -1,15 +1,10 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import './Cadastro.css'
 import Divider from '@mui/material/Divider';
 import { useNavigate } from 'react-router-dom';
 export default function Cadastro() {
     
   const [imagemPerfil,setImagemPerfil]=useState('https://thumbs.dreamstime.com/b/imita%C3%A7%C3%A3o-do-fundo-transparente-69028332.jpg')
-  const navigate = useNavigate()
-  const onSelectFile=(e)=>{
-    setImagemPerfil(URL.createObjectURL(e.target.files[0]))
-  }
-
   const [nome,setNome]=useState('')
   const [email,setEmail]=useState('')
   const [senha,setSenha]=useState('')
@@ -20,54 +15,129 @@ export default function Cadastro() {
   const [bairro,setBairro]=useState('')
   const [numero,setNumero]=useState('')
   const [complemento,setComplemento]=useState('')
-
-  function validaCampoNulo(Id,campo) {
-    function invalido(id) {
-        console.log(document.getElementById(id))
-        document.getElementById(id).classList.remove('is-valid')
-        document.getElementById(id).classList.add('is-invalid')
-      }
-      function valido(id) {
-        document.getElementById(id).classList.remove('is-invalid')
-        document.getElementById(id).classList.add('is-valid')
-      }
-
-      if(campo === ''){
-        invalido(Id)
-      }else{
-            valido(Id)
-      }
+  const [profissao,setProfissao]=useState('')
+  const [descricao,setDescricao]=useState('')
+  const [rua,setRua]=useState('')
+  const navigate = useNavigate()
+  const onSelectFile=(e)=>{
+    setImagemPerfil(URL.createObjectURL(e.target.files[0]))
   }
 
-  function validaSenha(Id,campo) {
-    function invalido(id) {
-        console.log(document.getElementById(id))
-        document.getElementById(id).classList.remove('is-valid')
-        document.getElementById(id).classList.add('is-invalid')
-      }
-      function valido(id) {
-        document.getElementById(id).classList.remove('is-invalid')
-        document.getElementById(id).classList.add('is-valid')
-      }
+  async function setarCep(params) {
+    fetch(`https://viacep.com.br/ws/${cep}/json/`)
+      .then(r=>r.json())
+      .then(res=>{
+        setBairro(res.bairro)
+        setCidade(res.localidade)
+        setRua(res.logradouro)
+        setEstado(res.uf)
+        setComplemento(res.complemento)
+      })
+      .catch(res=>{
+        // setBairro('')
+        // setCidade('')
+        // setRua('')
+        // setEstado('')
+        // setComplemento('')
+      }) 
+ }
+ setarCep()
 
-      if (senha !== confirSenha) {
-        invalido(Id)
-      } else {
-        valido(Id)
-      }
-      if(campo === ''){
-        invalido(Id)
-      }else{
-            valido(Id)
-      }
-  }
   
+
+
+
+const [senhaNula,setSenhaNula]=useState(false)
+const [senhaDiferente,setSenhaDiferente]=useState(false)
+const [valido,setValido]=useState('')
+
+
   const enviar = (e)=>{
     //e.preventDefault()
-    validaCampoNulo('validationDefault01',nome)
-    validaSenha('senha',senha)
-    validaSenha('validationDefault07',confirSenha)
+
+    if (senha === '' && confirSenha === '') {
+      setValido('is-invalid')
+      setSenhaNula(true)
+    } else {
+      setValido('is-valid')
+      setSenhaNula(false)      
+      if (senha !== confirSenha  ) {
+        setValido('is-invalid')
+        setSenhaDiferente(true)
+      } else {
+        
+        setValido('is-valid')
+        setSenhaDiferente(false)
+          
+      }
+    }
+
+    const formdata = new FormData()
+    formdata.append('nome',nome)
+    formdata.append('email',email)
+    formdata.append('profissao',profissao)
+    formdata.append('cidade',cidade)
+    formdata.append('bairro',bairro)
+    formdata.append('estado',estado)
+    formdata.append('descricao',descricao)
+    formdata.append('avatar',imagemPerfil)
+    formdata.append('numero',numero)
+    formdata.append('cep',cep)
+
+
+
+
+      
+  // function validaCampoNulo(Id,campo) {
+  //   function invalido(id) {
+  //       document.getElementById(id).classList.remove('is-valid')
+  //       document.getElementById(id).classList.add('is-invalid')
+  //     }
+  //     function valido(id) {
+  //       document.getElementById(id).classList.remove('is-invalid')
+  //       document.getElementById(id).classList.add('is-valid')
+  //     }
+
+  //     if(campo === ''){
+  //       invalido(Id)
+  //       setSenhaNula(true)
+  //     }else{
+  //           valido(Id)
+  //           setSenhaNula(false)
+  //     }
+  // }
+
+  // function validaSenha(Id,campo) {
+  //   function invalido(id) {
+  //       console.log(document.getElementById(id))
+  //       document.getElementById(id).classList.remove('is-valid')
+  //       document.getElementById(id).classList.add('is-invalid')
+  //     }
+  //     function valido(id) {
+  //       document.getElementById(id).classList.remove('is-invalid')
+  //       document.getElementById(id).classList.add('is-valid')
+  //     }
+
+  //     if (senha !== confirSenha) {
+  //       invalido(Id)
+  //       setSenhaDiferente(true)
+  //     } else {
+  //       valido(Id)
+  //       setSenhaDiferente(false)
+  //     }
+  //     if(campo === ''){
+  //       invalido(Id)
+  //     }else{
+  //           valido(Id)
+  //     }
+  // }
+
+  //   validaCampoNulo('validationDefault01',nome)
+  //   validaSenha('senha',senha)
+  //   validaSenha('validationDefault07',confirSenha)
   }
+
+
  
   return (
     <div classNameName='cadastroContainer'>
@@ -90,7 +160,7 @@ export default function Cadastro() {
                         <label for="validationDefault01 " className="form-label">Nome</label>
                         
                         <input id="validationDefault01" onChange={(e=>setNome(e.target.value))} type="text" className="form-control"  required/>
-                        <div className="invalid-feedback">campo inválido</div>
+                        <div className="invalid-feedback">campo não pode ser nulo</div>
                     </div>
                     <div className="col-md-4">
                         <label for="validationDefaultUsername" className="form-label">Email</label>
@@ -98,7 +168,9 @@ export default function Cadastro() {
                         <span className="input-group-text" id="inputGroupPrepend2">@</span>
                         <input 
                           onChange={(e=>setEmail(e.target.value))}
-                          type="email" className="form-control is-invalid" id="validationDefaultUsername"  aria-describedby="inputGroupPrepend2" required/>
+                          type="email" className="form-control is-invalid" id="validationDefaultUsername"  aria-describedby="inputGroupPrepend2" required
+                        />
+                        <div className="invalid-feedback">email invalido</div>
                         </div>
                     </div>
                     <div className="col-md-6">
@@ -110,88 +182,50 @@ export default function Cadastro() {
 
 
                         <label for="senha" className="form-label">Senha</label>
-                        <input type="text" className="form-control" id="senha" onChange={e=>setSenha(e.target.value)} required/>
+                        <input type="text" className={`form-control ${valido}`} id="senha" onChange={e=>setSenha(e.target.value)} required/>
+                        {senhaNula && <div className="invalid-feedback">campo não pode ser nulo</div>}
+                        {senhaDiferente && <div className="invalid-feedback">senhas não conferem</div>}
                     </div>
                     <div className="col-md-3">
-                    <label for="validationDefault07" className="form-label">Confirmar senha</label>
-                        <input type="text" className="form-control" id="validationDefault07" onChange={(e=>setConfirmsenha(e.target.value))} required/>
-                        <div className="invalid-feedback">campo inválido</div>
+                    <label for="confirmaSenha" className={`form-label`}>Confirmar senha</label>
+                        <input type="text" className={`form-control ${valido}`} id="confirmaSenha" onChange={(e=>setConfirmsenha(e.target.value))} required/>
+                        {senhaNula && <div className="invalid-feedback">campo não pode ser nulo</div>}
+                        {senhaDiferente && <div className="invalid-feedback">senhas não conferem</div>}
                     </div>
 
                     <Divider sx={{margin:'30px 0px'}}/>
 
                     <h3>Localidade</h3>
-                    {/* <div className="col-md-3">
-                        <label for="validationDefault04" className="form-label">Genero</label>
-                        <select className="form-select" id="validationDefault04" required>
-                        <option selected disabled value="">...</option>
-                        <option>Masculino</option>
-                        <option>Feminino</option>
-                        </select>
-                    </div> */}
-
+ 
                     <div className='col-md-3'>
                         <label for="cep" className="form-label">Cep</label>
                         <input type="text" className="form-control" id="cep" onChange={e=>setCep(e.target.value)} required/>
                     </div>
                     <div className='col-md-3'>
                         <label for="cidade" className="form-label">Cidade</label>
-                        <input type="text" className="form-control" id="cidade" onChange={e=>setCidade(e.target.value)} required/>
+                        <input type="text" value={cidade} className="form-control" id="cidade" onChange={e=>setCidade(e.target.value)} required/>
                     </div>
-                    <div className='col-md-6'>
+                    <div className='col-md-3'>
                         <label for="estado" className="form-label">Estado</label>
-                        <input type="text" className="form-control" id="estado" onChange={e=>setEstado(e.target.value)} required/>
+                        <input type="text" className="form-control" id="estado" value={estado} onChange={e=>setEstado(e.target.value)} required/>
+                    </div>
+                    <div className='col-md-3'>
+                        <label for="rua" className="form-label">rua</label>
+                        <input type="text" value={rua} className="form-control" id="rua" onChange={e=>setRua(e.target.value)} required/>
                     </div>
 
                     <div className='col-md-4'>
                         <label for="bairro" className="form-label">Bairro</label>
-                        <input type="text" className="form-control" id="bairro" onChange={e=>setBairro(e.target.value)} required/>
+                        <input type="text" className="form-control" id="bairro" value={bairro} onChange={e=>setBairro(e.target.value)} required/>
                     </div>
                     <div className='col-md-4'>
                         <label for="numero" className="form-label">Numero</label>
-                        <input type="text" className="form-control" id="numero" onChange={e=>setNumero(e.target.value)} required/>
+                        <input type="text" className="form-control" id="numero" value={numero} onChange={e=>setNumero(e.target.value)} required/>
                     </div>
                     <div className='col-md-4'>
                         <label for="complemento" className="form-label">Complemento</label>
-                        <input type="text" className="form-control" id="complemento" onChange={e=>setComplemento(e.target.value)} required/>
+                        <input type="text" className="form-control" id="complemento" value={complemento} onChange={e=>setComplemento(e.target.value)} required/>
                     </div>
-
-
-                    {/* <div className="col-md-3">
-                        <label for="validationDefault04" className="form-label">Cor</label>
-                        <select className="form-select" id="validationDefault04" required>
-                        <option selected disabled value="">...</option>
-                        <option>Branco(a)</option>
-                        <option>Pardo(a)</option>
-                        <option>Negro(a)</option>
-                        <option>Loiro(a)</option>
-                        <option>Moreno(a)</option>
-                        </select>
-                    </div>
-
-                    <div className="col-md-3">
-                    <label for="validationDefault04" className="form-label">Altura</label>
-                        <select className="form-select" id="validationDefault04" required>
-                        <option selected disabled value="">...</option>
-                        <option>abaixo de 1,60m</option>
-                        <option>1,60m a 1,70m</option>
-                        <option>1,70m a 1,80m</option>
-                        <option>1,80m a 1,90m</option>
-                        <option>acima de 1,90m</option>
-                        </select>                        
-                    </div>
-
-                    <div className="col-md-3">
-                    <label for="validationDefault04" className="form-label">Corpo</label>
-                        <select className="form-select" id="validationDefault04" required>
-                        <option selected disabled value="">...</option>
-                        <option>magro</option>
-                        <option>comum</option>
-                        <option>atletico</option>
-                        <option>um pouco acima do peso</option>
-                        <option>muito acima do peso</option>
-                        </select>                        
-                    </div> */}
 
                 </div>              
                 <label for="complemento" className="form-label">Descreva o que faz</label>
--- src/Login/Login.js
+++ src/Login/Login.js
@@ -1,13 +1,142 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import './Login.css'
-import {useNavigate} from 'react-router-dom'
+
+import Button from '@mui/material/Button';
+import IconButton from '@mui/material/IconButton';
+import OutlinedInput from '@mui/material/OutlinedInput';
+import InputLabel from '@mui/material/InputLabel';
+import InputAdornment from '@mui/material/InputAdornment';
+import FormControl from '@mui/material/FormControl';
+import TextField from '@mui/material/TextField';
+import Visibility from '@mui/icons-material/Visibility';
+import VisibilityOff from '@mui/icons-material/VisibilityOff';
 export default function Login() {
-  const navigate = useNavigate()
+
+  const [values, setValues] = React.useState({showPassword: false});
+  const [ErroDeSenha, setErroDeSenha] = useState(false)
+  const [ErroDeEmail, setErroDeEmail] = useState(false)
+  const [LabelEmail, setLabelEmail] = useState('email')
+  const [LabelSenha, setLabelSenha] = useState('senha')
+  const [email, setemail] = useState('')
+  const handleChange = (prop) => (event) => {
+    setValues({ ...values, [prop]: event.target.value });
+  };
+
+  const handleClickShowPassword = () => {
+    setValues({
+      ...values,
+      showPassword: !values.showPassword,
+    });
+  };
+
+  const handleMouseDownPassword = (event) => {
+    event.preventDefault();
+  };
+
+
+  const usuarios = [
+    {
+      email:"fabio@gmail.com",
+      senha:"fabio"
+    },
+    {
+      email:"ruth@gmail.com",
+      senha:"ruth"
+    },
+    {
+      email:"ana@gmail.com",
+      senha:"ana"
+    },
+  ]
+
+  const logar = ()=>{
+      var usuario = usuarios.find(use=>{
+        if (use.email === email && use.senha === values.password) {
+          return use
+        }else{
+          return false
+        }
+      })
+
+      var existeEmail = usuarios.find(e=>{
+        if (e.email === email) {
+          return true
+        }else{
+          return false
+        } 
+      })
+      
+      var existeSenha = usuarios.find(e=>{
+        if (e.senha === values.password) {
+          return true
+        }else{
+          return false
+        } 
+      })
+
+      if(!existeEmail){
+        setErroDeEmail(true)
+        setLabelEmail('usuario inexistente')
+      }else{
+        setErroDeEmail(false)
+        setLabelEmail('email')
+      }
+
+      if (!existeSenha) {
+        setErroDeSenha(true)
+        setLabelSenha('senha invalida')
+      } else {
+        setErroDeSenha(false)
+        setLabelSenha('senha')
+      }
+
+
+    if(usuario){
+      alert("usuario(a) "+usuario.email+" conectado com sucesso")
+    }
+  }
+
   return (
-    <div>
-      <h1>tela de login</h1>
-      <div>a desenvolver</div>
-      <button className='btn btn-primary' onClick={()=>navigate('/cadastro')}>va para tela de cadastro</button>
+    <div className='loginContainer'>
+      <h1 >Login</h1>
+      <div className='loginBody'>
+        <TextField
+           error={ErroDeEmail}
+           label={'email'}
+           size='small'
+           onChange={e=>setemail(e.target.value)}
+        />
+        {ErroDeEmail &&
+           <div className='labelEmail'>{LabelEmail}</div>
+        }
+        <FormControl sx={{ marginTop:"20px"}} variant="outlined" size='small'>
+          <InputLabel htmlFor="outlined-adornment-password">senha</InputLabel>
+          <OutlinedInput
+            error={ErroDeSenha}
+            id="outlined-adornment-password"
+            type={values.showPassword ? 'text' : 'password'}
+            value={values.password}
+            onChange={handleChange('password')}
+            endAdornment={
+              <InputAdornment position="end">
+                <IconButton
+                  aria-label="toggle password visibility"
+                  onClick={handleClickShowPassword}
+                  onMouseDown={handleMouseDownPassword}
+                  edge="end"
+                >
+                  {values.showPassword ? <VisibilityOff /> : <Visibility />}
+                </IconButton>
+              </InputAdornment>
+            }
+            label='password'
+          />
+        </FormControl>
+        {ErroDeSenha &&
+          <div className='labelSenha'>{LabelSenha}</div>
+        }
+        <Button sx={{marginTop:"20px"}} variant="contained" onClick={logar}>Logar</Button>
+      </div>
     </div>
   )
 }
